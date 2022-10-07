504f79408e96b53d347853fa438e2c861faa1cfb
Andre Soares <andre.soarestc13@gmail.com>
2022-10-07 17:49:20 +0100

    Completes the function that normalizes a polynomial

--- project1.hs
+++ project1.hs
@@ -2,7 +2,7 @@ import Data.List
 
 type Mon = (Int, String, Int) -- monomial where the first member is the number, the second the variable and the third the exponent, ex 7*y^2 = (7,"y",2)
 type Poly = [Mon]
-
+--pol ex : [(0,"x",2),(2,"y",0),(5,"z",0),(1,"y",0),(7,"y",2)]
 
 getExp :: Mon -> Int
 getExp (_,_,x) = x
@@ -22,8 +22,20 @@ getMonStr m = (show (getNum m) ++ "*" ++ (getVar m) ++ (if (getExp m > 1) then (
 sumMon :: Mon -> Mon -> Mon
 sumMon m1 m2 = (getNum m1 + getNum m2,getVar m1, getExp m1)
 
---normPol :: Poly -> Poly
---normPol p = foldl (\acc x -> if ((getVar x) == (getVar acc)) then acc:(sumMon acc x) else acc:x) [(0,"",0)] p
+normPol :: Poly -> Poly --Normalizes the Polynomial by removing zeros, sorting and adding all of the monomials that can be summed
+normPol p = foldl (\acc m -> if (length (filterVarAndExp acc m) == 0) then acc ++ [m] else (reverseFilterVarAndExp acc m) ++ [sumMon m (head (filterVarAndExp acc m))]  ) [] (sortPol (removeZeros p))
 
-sortPol :: Poly -> Poly
+sortPol :: Poly -> Poly -- Sorts the Polynomial first by the letter of the variable in alphabaetic order and then by the exponent in descending order
 sortPol p = sortBy (\(_,_,a) (_,_,b) -> flip compare a b) (sortBy (\(_,a,_) (_,b,_) -> compare a b) p)
+
+
+removeZeros :: Poly -> Poly -- Removes monomials containing 0 like 0y^2
+removeZeros p = filter (\(a,_,_) -> a /= 0) p
+
+
+filterVarAndExp :: Poly -> Mon -> Poly --Returns a polynomial that may only contains only the monomials that have the same variable and exponent as the one given
+filterVarAndExp p m = filter (\(_,b,c) -> (b == (getVar m)) && (c == (getExp m))) p
+
+reverseFilterVarAndExp :: Poly -> Mon -> Poly --The reverse of function filterVarAndExp, this one returns a Polynomial without any monomials that contain the same variable and exponent as the one given
+reverseFilterVarAndExp p m = filter (\(_,b,c) -> (b /= (getVar m)) || (c /= (getExp m))) p
+
