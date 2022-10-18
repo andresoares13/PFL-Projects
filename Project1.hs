d14d787964cb4c768eda1239dde06ac74c254501
Andre Soares <andre.soarestc13@gmail.com>
2022-10-18 20:18:17 +0100

    adds tests file

--- Project1.hs
+++ Project1.hs
@@ -1,3 +1,5 @@
+module Project1 (programUI,uiNormPol,uiSumPol,uiDerivePol,uiMulPol) where
+
 import Data.List
 import Data.Char
 
@@ -236,6 +238,7 @@ polyParse s = createPol (strSplit (removeSpaces s))
 
 -- User Interface Section
 
+
 programUI :: IO ()
 programUI = do
   putStr "\n\nPolynomial Operations Calculator \n\n\n What would you like to do? \n\n 1. Normalize a polynomial \n 2. Sum two polynomials \n 3. Multiply two polynomials \n 4. Derive a polynomial\n\n"
@@ -253,29 +256,50 @@ programUI = do
     else
       putStr "\nThank you for using this program!\n"  
   else 
-    putStr "\nNot implemented yet\n"   
+    if (option == (show 2)) then 
+      do
+      putStr "\nPlease write the two polynomials you would like to sum in the form 'ax^2 + bx + c'\n\n"
+      poly1 <- getLine
+      poly2 <- getLine
+      uiSumPol (polyParse poly1) (polyParse poly2)
+      putStr "\nWould you like to go back?\n[y/n]\n"
+      
+      back <- getLine
+      if (back == "y") then
+        programUI
+      else
+        putStr "\nThank you for using this program!\n"
+    else 
+      if (option == (show 3)) then
+        do
+        putStr "\nPlease write the two polynomials you would like to multiply in the form 'ax^2 + bx + c'\n\n"
+        poly1 <- getLine
+        poly2 <- getLine
+        uiMulPol (polyParse poly1) (polyParse poly2)
+        putStr "\nWould you like to go back?\n[y/n]\n"
+        
+        back <- getLine
+        if (back == "y") then
+          programUI
+        else
+          putStr "\nThank you for using this program!\n"
+      else    
+        if (option == (show 4)) then
+          do
+          putStr "\nPlease write the polynomials you would like to derive in the form 'ax^2 + bx + c' and the variable with respect to which you want to derivate\n\n"
+          poly1 <- getLine
+          var <- getLine
+          uiDerivePol (head var) (polyParse poly1)
+          putStr "\nWould you like to go back?\n[y/n]\n"
+          
+          back <- getLine
+          if (back == "y") then
+            programUI
+          else
+            putStr "\nThank you for using this program!\n"
+        else     
+          putStr "\nThank you for using this program!\n"   
 
 
 
-
---Tests Section
-
---test of norm: 
-              --uiNormPol[(0,"x",[2]),(2,"y",[1]),(5,"z",[1]),(1,"y",[1]),(7,"y",[2])
-              --uiNormPol (polyParse "8x^2 +  9y  -4z^3 +7yx-7yx+2")
--- test of sum: 
-              --uiSumPol [(3,"x",[2]),(6,"x",[1]),(4,"y",[2]),(5,"x",[1])] [(7,"x",[1]),((-6),"y",[2]),(4,"x",[1])]
-              --uiSumPol [(3,"x",[2]),(6,"x",[1]),(4,"y",[2]),(5,"x",[1])] [(7,"x",[1]),((-6),"y",[2]),(4,"xy",[1,2])]
-              --uiSumPol [(7,"x",[1]),((-6),"y",[2]),(4,"x",[1]),(5,"~",[0])] [(1,"y",[1])]
-              --uiSumPol [(10,"x",[2]),((-10),"y",[2]),((-1),"x",[1]),((-10),"x",[2]),(5,"~",[0])] [(5,"z",[6]),(10,"y",[2]),((-19),"~",[0]),(4,"x",[19]),((-7),"w",[4])]
-              --uiSumPol (mulPol [(2,"x",[2]),(3,"y",[1])] [(3,"y",[3]),(4,"zx",[1,1])]) [(2,"xzy",[1,1,1])
--- Test of mul:
-              --uiMulPol [(2,"x",[2]),(3,"y",[1])] [(3,"y",[3]),(4,"zx",[1,1])]
-
--- test of derivation:
-              --uiDerivePol 'x' [(7,"x",[1]),((-6),"y",[2]),(4,"x",[1]),(5,"~",[0])] 
-              --uiDerivePol 'x' [(2,"xy",[2,3]),(4,"x",[2]),(8,"y",[1]),((-9),"zx",[1,1])]
-
--- test of everything together (even parsing):
-              --uiSumPol (mulPol (derivePoly 'x' (normPol (polyParse "8x^2 +  9y  -   4z^3 +7yx-7yx+0"))) (polyParse "17yx+   5y^2")) (polyParse "80y^2x + 42")              
 
